e951563b70a16a7cc9274768b9f617b3bf11be94
FaithChepkemoi <faithhope017@gmail.com>
2024-12-16 15:00:40 +0300

    fecth

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded',() => {
     fetchMovies();
+
+    function fetchMovies(){
+        fetch('http://localhost:3000/films')
+        .then(response => response.json())
+        .then(movies =>{
+            const filmsList = document.getElementById('films')
+            movies.forEach(movie => {
+                const li = document.createElement('li');
+                li.className = 'film item';
+                li.innerText = movie.title;
+                li.addEventListener('click', () => displayMovieDetails(movie));
+                filmsList.appendChild('li');
+            });
+            displayMovieDetails(movies[0]); // Display first movie by default
+        })
+
+    }
 }
 )
